e96c74dba799a5312bc78d87c859651220212adb
suyash <smhj022@gmail.com>
2021-08-07 01:19:08 +0530

    learn insertion_sort

--- helloworld/sorting.h
+++ helloworld/sorting.h
@@ -23,20 +23,39 @@ int bubble_sort() {
 }
 
 
-int selection_sort(){
+int selection_sort() {
     int len = 11;
-    int arr[len] = {47, 65, 3, 500, 111, 45 , 45, 123, 55, 23, 5};
-    for (int current=0; current < len; current++){
+    int arr[len] = {47, 65, 3, 500, 111, 45, 45, 123, 55, 23, 5};
+    for (int current = 0; current < len; current++) {
         int min_value_index = current;
-        for(int next=current+1; next < len; next++){
-            if (arr[next] < arr[min_value_index]){
+        for (int next = current + 1; next < len; next++) {
+            if (arr[next] < arr[min_value_index]) {
                 min_value_index = next;
             }
         }
         int temp = arr[min_value_index];
         arr[min_value_index] = arr[current];
         arr[current] = temp;
+    }
+    for (int k = 0; k < len; k++) {
+        cout << arr[k] << "\n";
+    }
+    return 0;
+}
+
+
+int insertion_sort() {
+    int len = 11;
+    int arr[len] = {47, 65, 3, 500, 111, 45, 45, 123, 55, 23, 5};
+    for (int external = 1; external < len; external++) {
+        int current_value = arr[external];
+        int internal = external - 1;
+        while(internal >= 0 && arr[internal]>current_value){
+            arr[internal+1]=arr[internal];
+            internal--;
         }
+        arr[internal+1] = current_value;
+    }
     for (int k = 0; k < len; k++) {
         cout << arr[k] << "\n";
     }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include "helloworld/bubble_sort.h"
+#include "helloworld/sorting.h"
 
 int main() {
 //    bubble_sort();
-    selection_sort();
+//    selection_sort();
+      insertion_sort();
     return 0;
 }
