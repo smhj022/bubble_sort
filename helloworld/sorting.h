b9651603603e66d373d6649ac96685c024824acb
suyash <smhj022@gmail.com>
2021-08-08 01:48:02 +0530

    Power and factorial function using RECURSSION

--- helloworld/sorting.h
+++ helloworld/sorting.h
@@ -6,6 +6,14 @@
 
 using namespace std;
 
+int print_array(int arr[], int len){
+    for (int value = 0; value < len; value++) {
+        cout << arr[value] << "\n";
+    }
+    return 0;
+}
+
+// Bubble sort
 int bubble_sort() {
     int value = 0;
     int arr[10] = {47, 65, 3, 500, 111, 45, 123, 45, 23, 5};
@@ -22,7 +30,7 @@ int bubble_sort() {
     return 0;
 }
 
-
+// Selection sort
 int selection_sort() {
     int len = 11;
     int arr[len] = {47, 65, 3, 500, 111, 45, 45, 123, 55, 23, 5};
@@ -37,16 +45,14 @@ int selection_sort() {
         arr[min_value_index] = arr[current];
         arr[current] = temp;
     }
-    for (int k = 0; k < len; k++) {
-        cout << arr[k] << "\n";
-    }
+    print_array(arr, len);
     return 0;
 }
 
-
+// Insertion Sort
 int insertion_sort() {
     int len = 11;
-    int arr[len] = {47, 65, 3, 500, 111, 45, 45, 123, 55, 23, 5};
+    int arr[len] = {47, 65, 3, 500, 111, 77, 45, 123, 55, 23, 5};
     for (int external = 1; external < len; external++) {
         int current_value = arr[external];
         int internal = external - 1;
@@ -56,8 +62,43 @@ int insertion_sort() {
         }
         arr[internal+1] = current_value;
     }
-    for (int k = 0; k < len; k++) {
-        cout << arr[k] << "\n";
-    }
+    print_array(arr,len);
     return 0;
 }
+
+// Power function using for series
+//int power(int no, int exponent) {
+//    int result = 1;
+//    for(int n=exponent; n>0; n--){
+//        result = result * no;
+//    }
+//
+//    cout << result ;
+//}
+
+
+// Power function using recursion
+int power(int no, int exponent) {
+
+    if (exponent == 0) {
+        return 1;
+    }
+    if (no == 0) {
+        return 0;
+    }
+
+    return no * power(no, exponent - 1);
+}
+
+// Factorial of a number using recursion
+long int fact(int no) {
+    if(no==0){
+        return 1;
+    }
+    if(no==1){
+        return 1;
+    }
+    return no*fact(no-1);
+}
+
+
